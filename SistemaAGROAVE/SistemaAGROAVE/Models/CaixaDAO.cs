fef4967127fab12a0f39c82ea9a4fa14f1788e51
Tainara <84594015+TainaraLL@users.noreply.github.com>
2023-10-18 04:08:53 -0400

    agro

---
+++ SistemaAGROAVE/SistemaAGROAVE/Models/CaixaDAO.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using SistemaAGROAVE.DataBase;
+using
+
+namespace SistemaAGROAVE.Models
+{
+    internal class CaixaDAO : IDAO<Caixa>
+    {
+        private static Conexao conn;
+    }
+    {
+    }
+}
